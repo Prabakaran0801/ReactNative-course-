16c89f25c78f25ba5d0522d50c15d71130441547
Prabakaran <karanpk0108@gmail.com>
2024-08-23 18:09:45 +0530

    handling Tasks with state and adding styles

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
+  function taskInputHandler(text) {
+    setTaskText(text);
+  }
+
+  function taskButtonHndler() {
+    setTasks((currenttasks) => [...currenttasks, taskText]);
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.textContainer}>
-        <TextInput style={styles.textInput} placeholder="Add your Task" />
-        <Button title="Add Task" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add your Task"
+          onChangeText={taskInputHandler}
+        />
+        <Button title="Add Task" onPress={taskButtonHndler} />
       </View>
-      <View>
-        <Text>Your Task...</Text>
+      <View style={styles.taskContainer}>
+        {tasks.map((task) => (
+          <View style={styles.taskItem} key={task}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -16,17 +34,38 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    backgroundColor: "#aed6dc",
   },
   textContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+    borderBottomColor: "#4a536b",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
+    borderColor: "#4a536b",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  taskContainer: {
+    flex: 5,
+    paddingTop: 10,
+  },
+  taskItem: {
+    margin: 6,
+    padding: 6,
+    borderRadius: 8,
+    backgroundColor: "#ff9a8d",
+  },
+  taskText: {
+    color: "white",
   },
 });
